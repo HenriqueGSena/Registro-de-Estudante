f09d7d4bb2186a751b137497a377cd38a226ac1a
HenriqueGSena <carloshenrique3250@gmail.com>
2022-02-08 17:51:40 -0300

    Acertando Repository e testando método post

--- studantRegistration/src/main/java/com/springRegister/studantRegistration/repository/StudentRepository.java
+++ studantRegistration/src/main/java/com/springRegister/studantRegistration/repository/StudentRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     
-    @Query("SELECT s FROM s WHERE s.email = ?1")
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
