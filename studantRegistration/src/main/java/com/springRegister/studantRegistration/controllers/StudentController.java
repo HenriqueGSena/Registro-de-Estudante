663c210a3d54256ceffdec1d0b8586dad47b9ead
HenriqueGSena <carloshenrique3250@gmail.com>
2022-02-08 17:05:44 -0300

    Adicionando método Post

--- studantRegistration/src/main/java/com/springRegister/studantRegistration/controllers/StudentController.java
+++ studantRegistration/src/main/java/com/springRegister/studantRegistration/controllers/StudentController.java
@@ -7,6 +7,8 @@ import com.springRegister.studantRegistration.student.Student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,11 @@ public class StudentController {
     public List<Student> getStudents() {
         
         return studentService.getStudents();
+    }
+
+    @PostMapping("/registration")
+    public void registerNewStudent(@RequestBody Student student) {
+
+        studentService.addNewStudent(student);
     }
 }
